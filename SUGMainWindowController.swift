1371c60f85cc3a93af0032bb9c28fe294cebfd32
John Brayton <jbrayton@goldenhillsoftware.com>
2023-08-31 16:09:30 -0400

    Created a Window Controller and View Controller. Moved a bunch of code out of the App Delegate and into the Window Controller and View Controller.

---
+++ SUGMainWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  SUGMainWindowController.swift
+//  CustomMenus
+//
+//  Created by John Brayton on 8/31/23.
+//
+
+import AppKit
+
+class SUGMainWindowController : NSWindowController {
+    
+    @IBOutlet var imageView: NSImageView!
+    @IBOutlet var searchField: NSTextField!
+
+    override func windowDidLoad() {
+        
+    }
+
+}
